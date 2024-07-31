60eecd97955caf6627f141cee48354ce9f02027c
Praveen Kumar K <praveenkali26@gmail.com>
2024-07-31 21:53:16 +0530

    largest substring problem

--- largestSubString.js
+++ largestSubString.js
@@ -4,25 +4,22 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-    if(s.length<=1) return s.length;
+    // if(s.length<=1) return s.length;
 
     let max =0, l=0;
     //let seen = new Map();
      let seen = {}
     for(let r=l; r<s.length; r++){
         let curr = s[r];
-        console.log(curr)
         //let prev = seen.get(curr);
          let prev = seen[curr]
-        console.log(prev > 1);
         if(prev >= l){
             l=prev+1
         }
-
         //seen.set(curr, r) //set map with key is ele and value is the index
          seen[curr] = r
         max = Math.max(max, r-l+1)
     }
     return max;
 };
-console.log(lengthOfLongestSubstring("epwwkw"));
+console.log(lengthOfLongestSubstring("pwwkew"));
